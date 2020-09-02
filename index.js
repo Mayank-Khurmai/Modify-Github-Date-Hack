9f3fbc9e45a558af1b4073c562d026f53b0ae8d6
Mayank-Khurmaim <mayankkhurmai8@gmail.com>
2020-09-02 16:55:53 +0530

    Final Commit with the Bot

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const jsonfile = require("jsonfile");
 const moment = require("moment");
 const simplegit = require("simple-git");
+const random = require("random");
+var random_number = random.int(0,365);
 
 const FILE_PATH = "./data.json";
-const DATE = moment().subtract(4,"d").format();
 
-const json_data = {
-    date : DATE
+
+// jsonfile.writeFile(FILE_PATH, json_data);
+// To obtain final command ==> git commit --date="2020-08-29T15:49:54+05:30"
+// simplegit().add([FILE_PATH]).commit(DATE, {'--date':DATE}).push();
+// For working synchronously , we can write the above code as with loop
+
+
+
+var i=0;
+var commit_loop = setInterval(start_commit, 10000);
+
+function start_commit(){
+    i++;
+    var DATE = moment().subtract(121+i,"d").format();
+
+    const json_data = {
+        date : DATE
+    }
+    
+    jsonfile.writeFile(FILE_PATH, json_data, ()=>{
+        simplegit().add([FILE_PATH]).commit(DATE, {'--date':DATE}).push();
+        if(i==3){ stop_commit() }
+    });
 }
 
-jsonfile.writeFile(FILE_PATH, json_data);
-// git commit --date="2020-08-29T15:49:54+05:30"
-simplegit().add([FILE_PATH]).commit(DATE, {'--date':DATE}).push();
+function stop_commit(){
+        clearInterval(commit_loop);
+}
